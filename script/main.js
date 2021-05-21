8cb6711511b6c82972e757707e31a83546278e7f
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-05-21 18:50:09 +0530

    Init

---
+++ script/main.js
@@ -0,0 +1,9 @@
+$("#video-play").hover(
+    function(){
+    $("#video-play").css("width", "100%");
+    $("#video-play").css("height", "100%");
+    },
+    function(){
+        $("#video-play").css("width", "100%");
+        $("#video-play").css("height", "100%");
+  });
